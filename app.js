f9c59e210a70beb4d5482862b682e5c14132a31f
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2017-03-11 23:16:24 +0800

    WIP: markup and styling for homepage

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', all);
