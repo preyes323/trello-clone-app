2c5c00311f0f2ec871905088f740e3ec5f6e3617
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2017-03-18 22:08:19 +0800

    WIP: popups for dashboard

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -24,7 +24,7 @@ const App = {
     this.user = new User(user);
     this.navView = new Nav({ model: this.user});
     this.navView.render();
-    this.loadUserBoards();
+    // this.loadUserBoards();
   },
 };
 
