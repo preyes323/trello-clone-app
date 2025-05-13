c99b8f51ac84cc5d27eedf003fbd79763a806e1b
Victor Reyes <vpaoloreyes@gmail.com>
2025-05-13 11:41:51 +0800

    Update sample.rb

--- sample.rb
+++ sample.rb
@@ -1,1 +1,29 @@
-puts array1.eql? array2
+class CircularBuffer
+  def initialize(size)
+    @buffer = [nil] * size
+    @next_position = 0
+    @oldest_position = 0
+  end
+
+  def put(object)
+    unless @buffer[@next_position].nil?
+      @oldest_position = increment(@next_position)
+    end
+
+    @buffer[@next_position] = object
+    @next_position = increment(@next_position)
+  end
+
+  def get
+    value = @buffer[@oldest_position]
+    @buffer[@oldest_position] = nil
+    @oldest_position = increment(@oldest_position) unless value.nil?
+    value
+  end
+
+  private
+
+  def increment(position)
+    (position + 1) % @buffer.size
+  end
+end
